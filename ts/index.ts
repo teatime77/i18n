aa8ab1575c9965549c9769de6cf6859e15d68e1a
teatime77 <teatime77@live.jp>
2026-08-13 22:48:03 +0900

    use vite server

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,8 @@
-export { AbstractSpeech, getIdFromText, initI18n, AppMode, appMode, Readable, Reading, token, TT, TTs, upperLatinLetters, isEdge, EngTextToId, setTextLanguageCode, loadTranslationMap, getEngTexts } from "./reading.js";
+export { AbstractSpeech, getIdFromText, initI18n, AppMode, appMode, Reading, token, TT, TTs, upperLatinLetters, isEdge, EngTextToId, setTextLanguageCode, loadTranslationMap, getEngTexts } from "./reading.js";
+export type { Readable } from "./reading.js";
 export { append, check, assert, MyError, msg, getPlayMode, PlayMode, range, sleep, fetchText, fetchTextResponse, fetchJson, remove, shuffle, getRandomInt, zip } from "./util.js";
 export { $, $div, $dlg, $inp, $sel, sum, arrayFill, last, range2, permutation, areSetsEqual, isSubSet, intersection, unique, setPlayMode, parseURL, downloadJson } from "./util.js";
 export { Speech, setOnSpeak, langCodeList, voiceLanguageCode, initSpeech, setVoiceLanguageCode, cancelSpeech } from "./speech.js";
 export { Vec2, Vec3 } from "./vector.js";
-export { pixUI, AbstractUIAttr, AbstractUI, Padding, IGrid, initGrid, setRowColIdxOfChildren, setMinSizeGrid, getDocumentSize, layoutGrid } from "./ui.js";
+export { pixUI, AbstractUI, Padding, initGrid, setRowColIdxOfChildren, setMinSizeGrid, getDocumentSize, layoutGrid } from "./ui.js";
+export type { AbstractUIAttr, IGrid } from "./ui.js";
